56fe1c42b512bca8c5a152d2da95bf2b2b7d1a6a
Laboratorio 02 <laboratoriounam-apple02@macbook-pro-de-laboratorio-2.local>
2019-03-06 13:45:58 -0600

    Initial Commit

---
+++ Proyecto06032019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proyecto06032019
+//
+//  Created by Laboratorio UNAM-Apple 02 on 06/03/19.
+//  Copyright © 2019 AppleLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
